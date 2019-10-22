f312321b99e365480e06da0e93f2831cf4fec892
unknown <hetianfu3@360buyad.local>
2019-10-22 15:11:51 +0800

    I. remove .cpp extension; II. remove modules ,  and

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ except ImportError:
 	from distutils.core import setup, Extension
 
 # requiring C++ here for Windows support.
-c1=Extension('_geohash',
-	sources=['src/geohash.cpp',],
-	define_macros = [('PYTHON_MODULE',1),])
+# c1=Extension('_geohash',
+# 	sources=['src/geohash.cpp',],
+#       define_macros = [('PYTHON_MODULE',1),])
 
 setup(name='python-geohash',
 	version='0.8.5',
 	description='Fast, accurate python geohashing library',
 	author='Hiroaki Kawai',
 	url='http://code.google.com/p/python-geohash/',
-	py_modules=['geohash','quadtree','jpgrid','jpiarea'],
-	ext_modules = [c1]
+	py_modules=['geohash']
+	# py_modules=['geohash','quadtree','jpgrid','jpiarea']
+	# ext_modules = [c1]
 )
